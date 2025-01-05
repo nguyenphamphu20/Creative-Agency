7c04ad245ceb55642545d22b7fb9c38770979bf3
Phú Nguyễn <nguyenphamphu20@gmail.com>
2025-01-05 22:34:42 +0700

    add header

---
+++ main.js
@@ -0,0 +1,3 @@
+const listNavbar = document.querySelector('.navbar__list');
+const listNavbarTablet = document.querySelector('.navbar__list--tablet');
+listNavbarTablet.innerHTML = listNavbar.innerHTML;
