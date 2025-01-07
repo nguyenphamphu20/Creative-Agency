3f42873044f8d4d401c4748ec989f02989bf314b
Phú Nguyễn <nguyenphamphu20@gmail.com>
2025-01-07 21:45:43 +0700

    done header

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-const listNavbar = document.querySelector('.navbar__list');
-const listNavbarTablet = document.querySelector('.navbar__list--tablet');
-listNavbarTablet.innerHTML = listNavbar.innerHTML;
+// Hàm
+function copy(classOne, classTwo) {
+    const tempOne = document.querySelector(`.${classOne}`);
+    const tempTwo = document.querySelector(`.${classTwo}`);
+    if (tempOne && tempTwo) {
+        tempTwo.innerHTML = tempOne.innerHTML;
+    } else {
+        console.error("One of the elements was not found");
+    }
+}
+// Sủ dụng
+
+copy("navbar__list", "navbar__list--tablet");
+
+copy("footer-list","footer__list-tablet");
